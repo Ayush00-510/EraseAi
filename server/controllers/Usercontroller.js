e97835b75efecfc10e2ddbbe4357498439c688fa
Ayush00-510 <prajapatiayu1401@gmail.com>
2025-12-19 16:45:48 +0530

    Resturcturing project & Added Backend

---
+++ server/controllers/Usercontroller.js
@@ -0,0 +1,9 @@
+
+
+
+// API controller Function to manage clerk user with Database
+// https://localhost:4000/api/user/webhooks    ---->Webhooks End Point 
+
+const clerkWebhooks = async(req,res) =>{
+    
+}
---
+++ server/server.js
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import express from "express"
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+//App config
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+// Initialize middleware
+app.use(express.json())
+app.use(cors())
+
+
+// API route
+app.get('/', (req,res)=>res.send("API Working"))
+app.listen(PORT, ()=>console.log("Server is Running on port "+PORT))
