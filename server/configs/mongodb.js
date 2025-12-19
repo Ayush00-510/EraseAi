10ed7b6c282ca0603c053722a30ccd559713a7db
Ayush00-510 <prajapatiayu1401@gmail.com>
2025-12-19 20:19:39 +0530

    Added vercel and modified mongodb configs

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,12 +2,21 @@ import mongoose from "mongoose";
 
 const connectDB = async() =>{
 
-    mongoose.connection.on('connected', ()=>{
-        console.log("Database Connected");
-    })
+    try {
+
+        // Prevent multiple MongoDB connections
+        if (mongoose.connection.readyState >= 1) return;
 
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/EraseAi`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/EraseAi`);
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);
+        process.exit(1);
+    }
+
+
+
 }
 
 export default connectDB
